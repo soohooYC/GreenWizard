a988c86fa9b1c49e42013aa69214a2c124b51fab
soohoo <soohooyuchen@gmail.com>
2023-09-25 18:29:56 +0800

    nav

--- app/src/main/java/com/example/greenwizard/user.kt
+++ app/src/main/java/com/example/greenwizard/user.kt
@@ -2,6 +2,8 @@ package com.example.greenwizard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -11,7 +13,7 @@ class user : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
 
-        val navController = this.findNavController(R.id.myNavHostFragment)
+        val navController = this.findNavController(R.id.usernavHost)
         NavigationUI.setupActionBarWithNavController(this, navController)
 
         //Bottom Nav
@@ -29,6 +31,24 @@ class user : AppCompatActivity() {
                 }
             }
             false
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.feedback, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.usernavHost)
+        when (item.itemId) {
+            R.id.Feedback -> {
+                // Navigate to the Add Feedback fragment
+                navController.navigate(R.id.addFeedback)
+                return true
+            }
+            // Add more cases for other menu items if needed
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 }
