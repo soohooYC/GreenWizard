de5ffca507931478260426a6764f0323e3cb03de
soohoo <soohooyuchen@gmail.com>
2023-09-25 17:26:29 +0800

    add user activity & user nav

--- app/src/main/java/com/example/greenwizard/user.kt
+++ app/src/main/java/com/example/greenwizard/user.kt
@@ -3,7 +3,7 @@ package com.example.greenwizard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class News : AppCompatActivity() {
+class user : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
