b306ba2c3dd8d84331c46e511e05b205a805670d
soohoo <soohooyuchen@gmail.com>
2023-09-27 22:56:47 +0800

    logout

--- app/src/main/java/com/example/greenwizard/LocationSelectionAdminragment.kt
+++ app/src/main/java/com/example/greenwizard/LocationSelectionAdminragment.kt
@@ -1,6 +1,7 @@
 package com.example.greenwizard
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,9 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+
 
 
 class LocationSelectionAdminragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,12 +27,25 @@ class LocationSelectionAdminragment : Fragment() {
 
         val rabutton = view.findViewById<Button>(R.id.reportadmin)
         val rpabutton = view.findViewById<Button>(R.id.recycle_Pointadmin)
+        val logoutButton = view.findViewById<Button>(R.id.logoutButton)
 
         rabutton.setOnClickListener {
             view.findNavController().navigate(R.id.action_locationSelectionAdminragment_to_list_report_newFragment)
         }
         rpabutton.setOnClickListener{
             view.findNavController().navigate(R.id.action_locationSelectionAdminragment_to_listRecycle)
+        }
+
+        logoutButton.setOnClickListener{
+            // Sign out the user from Firebase Authentication
+            auth.signOut()
+
+            // Create an Intent to navigate to the LoginActivity
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+
+            // Start the LoginActivity
+            startActivity(intent)
+
         }
 
 
